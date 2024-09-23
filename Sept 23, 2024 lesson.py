23c02e6f1b5c72a7f3da4f560ee23676f98762b1
thehonored1ne <162032331+thehonored1ne@users.noreply.github.com>
2024-09-23 09:30:34 +0800

    Update Sept 23, 2024 lesson.py

--- Sept 23, 2024 lesson.py
+++ Sept 23, 2024 lesson.py
@@ -55,8 +55,6 @@ number = 1234
 message(1)
 print(number)
 
-'''
-
 def message(what, number):
     print("enter", what, "number", number)
     
@@ -64,9 +62,89 @@ message("telephone", 11)
 message("price", 5)
 message("number", "number")
 
+def my_function(a, b, c):
+    print(a, b, c)
+
+my_function(1,2,3)
+
+def introduction(first_name, last_name):
+    print("hello, my name is", first_name, last_name)
+    
+introduction("Luke", "skywalker")
+introduction("Jesse", "quick")
+introduction("Clark", "kent")
+
+def introduction(first_name, last_name):
+    print("hello, my name is", first_name, last_name)
+    
+introduction(first_name = "james", last_name = "bond")
+introduction(last_name = "skywalker", first_name = "luke")
+
+def adding(a,b,c):
+    print(a, "+", b, "+", c, "=", a + b + c)
+    
+adding(1,2,3)
+
+def adding(a,b,c):
+    print(a, "+", b, "+", c, "=", a + b + c)
+    
+adding(4,3, c = 2)
+
+def introduction(first_name, last_name = "smith"):
+    print("Hello, my name is", first_name, last_name)
+    
+introduction("james, Doe")
+introduction("Henry")
+
+def happy_new_year(wishes = True):
+    print("Three...")
+    print("two...")
+    print("one...")
+    if not wishes:
+        return
+        
+    print("happy new year!")
+happy_new_year()
 
 
+def boring_function():
+    return 123
+    
+x = boring_function()
 
+print("the boring_function has returned its result. It's; ", x)
+
+
+def boring_function():
+    print("'Boredom Mode' ON.")
+    return 123
+    
+print("this lesson is interesting")
+boring_function()
+print("this lesson is boring...")
+
+value = None
+if value is None:
+    print("sorry, you do not carry any value")
+
+def strange_function(n):
+    if(n % 2 == 0):
+        return True
+        
+print(strange_function(2))
+print(strange_function(1))
+
+def list_sum(lst):
+    s = 0
+    
+    for elem in lst:
+        s += elem
+        
+    return s
+
+print(list_sum([5,4,3]))
+
+'''
 
 
 
