caf769df82e180f8be8033b6818e356cd95b11ea
thehonored1ne <162032331+thehonored1ne@users.noreply.github.com>
2024-09-23 09:33:03 +0800

    Update Sept 23, 2024 lesson.py

--- Sept 23, 2024 lesson.py
+++ Sept 23, 2024 lesson.py
@@ -145,7 +145,15 @@ def list_sum(lst):
 print(list_sum([5,4,3]))
 
 '''
-
+def strange_list_fun(n):
+    strange_list = []
+    
+    for i in range(0,n):
+        strange_list.insert(0,i)
+        
+    return strange_list
+    
+print(strange_list_fun(5))
 
 
 
